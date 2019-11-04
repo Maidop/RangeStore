2402e6935fa6d595d2344dfbf686128bb9d431c5
Maikon Pscheidt <43589972+Maidop@users.noreply.github.com>
2019-11-04 18:58:15 -0300

    correção

---
+++ src/main/java/br/edu/utfpr/pb/range/controller/CompraController.java
@@ -0,0 +1,89 @@
+package br.edu.utfpr.pb.range.controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.utfpr.pb.range.model.Compra;
+import br.edu.utfpr.pb.range.service.CategoriaService;
+import br.edu.utfpr.pb.range.service.CompraService;
+import br.edu.utfpr.pb.range.service.FornecedorService;
+import br.edu.utfpr.pb.range.service.MarcaService;
+import br.edu.utfpr.pb.range.service.ProdutoService;
+
+@Controller
+@RequestMapping("compra")
+public class CompraController {
+	
+	@Autowired
+	private CompraService compraService;
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@Autowired
+	private MarcaService marcaService;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private FornecedorService fornecedorService;
+	
+	@GetMapping
+	public String list(@RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size,
+			Model model) {
+		int currentPage = page.orElse(1);
+		int pageSize = size.orElse(5);
+
+		Page<Compra> list = this.compraService.findAll(PageRequest.of(currentPage - 1, pageSize));
+
+		model.addAttribute("list", list);
+		model.addAttribute("categoria", categoriaService.findAll());
+		model.addAttribute("marca", marcaService.findAll());
+		model.addAttribute("produto", produtoService.findAll());
+		model.addAttribute("fornecedor", fornecedorService.findAll());
+
+		if (list.getTotalPages() > 0) {
+			List<Integer> pageNumbers = IntStream.rangeClosed(1, list.getTotalPages()).boxed()
+					.collect(Collectors.toList());
+			model.addAttribute("pageNumbers", pageNumbers);
+		}
+
+		return "compra/form";
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<?> save(
+			@RequestBody @Valid Compra entity,
+			BindingResult result, Model model,
+			RedirectAttributes attributes){
+		
+		if (result.hasErrors()) {
+			return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+		}
+		
+		compraService.save(entity);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+}
--- src/main/java/br/edu/utfpr/pb/range/service/impl/CompraServiceImpl.java
+++ src/main/java/br/edu/utfpr/pb/range/service/impl/CompraServiceImpl.java
@@ -2,11 +2,14 @@ package br.edu.utfpr.pb.range.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
 import br.edu.utfpr.pb.range.model.Compra;
 import br.edu.utfpr.pb.range.repository.CompraRepository;
+import br.edu.utfpr.pb.range.service.CompraService;
 
-public class CompraServiceImpl extends CrudServiceImpl<Compra, Long> {
+@Service
+public class CompraServiceImpl extends CrudServiceImpl<Compra, Long> implements CompraService {
 
 	@Autowired
 	private CompraRepository compraRepository;
